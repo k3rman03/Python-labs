cf13c5aa3fd465a710e8c374882f9026e6bc2c0b
Kerman <kermanr@gmail.com>
2025-03-25 00:49:54 -0400

    Commit as py

---
+++ Homework 2.py
@@ -0,0 +1,52 @@
+# Kerman Rodriguez
+# 03/22/2025
+# Fun game that prompts user for data to populate a list and then does fun things with the data
+
+
+def main():
+
+    #Create an empty list to store the answers in
+    dataList = []
+
+    #Declare a constant that stores your answer to the question (just 1 answer), and one to store the number of items to be entered (5)
+    qAnswer = ""
+    numItems = 5
+
+    #Declare and initialize a variable for play again
+    playAgain = "yes"
+
+    #Display a friendly attractive Title
+    print("Welcome to the Fun Game!")
+
+    #Display a short intro to the game and any rules they should know
+    print("\nThis game will ask you a question 5 times and store your answers in a list. Then we will do some fun things with the list.")
+       
+    #For every time the user said yes they do want to play again do the following:
+   
+    #Ask the user a question 5 times and add their answer from each time to a list      
+    #Display your answer to the question - using the constant in the output
+    
+       
+    #If your answer is also in the list display something like "I see we might know the same person as you wrote 'Your Answer' also!" (or whatever your question was about if not a name)
+    #Otherwise display something like "I see that you don't have a favorite person named 'Your Answer', let's add that name to our list" - and Add the name to the end of the list (or whatever your question was about if not a name)
+       
+    #Display "Now let's do some fun things with our list of (whatever the list is of):"
+    #Sort the list in ascending order using the sort method
+    #Display "Here is the list in alphabetical order 'list' "
+       
+    #Display "The last item in the list now is 'display last item in list' " using -1
+       
+    #Display "I can count how many items are in the list. There are 'num items in list'  items in the list" (using len)
+       
+    #Use an infinite loop (while True) to ask the user to enter an item to be removed from the list.
+    #if the item the user enters is in the list delete it and exit the loop (use break)
+    #if the item is not in the list display "Sorry, that item is not in the list, please try again" and keep asking for an item in the list
+       
+    #Display the number of items in the list now with a friendly sentence
+    #Loop through the list and display each item on its own line
+   
+    #Use 1 additional method we learned (not already used) or slicing to display something fun to the user
+ 
+    #Prompt user if they want to play again
+       
+    #Display a simple outro (like we have done in all lectures)
